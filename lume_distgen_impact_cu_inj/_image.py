f32d0d7f18bbbb9c08657031246383ce2c411e0a
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-10-19 15:51:11 -0700

    Fix image name

--- lume_distgen_impact_cu_inj/_image.py
+++ lume_distgen_impact_cu_inj/_image.py
@@ -1,4 +1,4 @@
 from lume_distgen_impact_cu_inj import __version__
 
 # Stanford Container Registry image
-IMAGE = f"scr.svc.stanford.edu/jgarra/lume-lcls-cu-inj-nn:v{__version__}"
+IMAGE = f"scr.svc.stanford.edu/jgarra/lume-distgen-impact-cu-inj:v{__version__}"
